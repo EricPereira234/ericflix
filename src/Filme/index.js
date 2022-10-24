b52458c486df6f6cfa8e494a82fdb54db646518b
Eric Pereira <92660469+EricPereira234@users.noreply.github.com>
2022-10-24 16:09:05 -0300

    teste 5

--- src/Filme/index.js
+++ src/Filme/index.js
@@ -1,7 +1,38 @@
+import "./Filme.css";
+import api from "../services/api";
+import { useEffect, useState } from 'react';
 
+export default function Filme() {
+    const [filmes, setFilmes] = useState([]);
 
-export default function Filme(){
-    return(
-        <h1>Filme</h1>
+    useEffect(()=>{
+        async function loadFilmes(){
+            const response = await api.get("/movie/popular", {
+                params:{
+                    api_key: "28fc232cc001c31e8a031f419d0a14ca",
+                    language: "pt-BR",
+                    page: 1,
+                   }
+            })
+            setFilmes(response.data.results.slice(0, 1));
+        }
+        loadFilmes();
+    },[]);
+
+    return (
+        <>
+            <div className="destaque" >
+            {filmes.map((filme) => {
+               
+               return (
+               <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+               );
+            }
+            )}
+           </div>
+            <h3>Lançamentos</h3>
+            <h3>Lançamentos</h3>
+            <h3>Lançamentos</h3>
+        </>
     )
 }
