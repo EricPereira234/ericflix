a68b8336b2a5f8b0cf3b24857d57c402282dae70
Eric Pereira <92660469+EricPereira234@users.noreply.github.com>
2022-10-26 11:51:47 -0300

    area dos filmes

--- src/Filme/index.js
+++ src/Filme/index.js
@@ -4,42 +4,75 @@ import { useEffect, useState } from 'react';
 
 export default function Filme() {
     const [filmes, setFilmes] = useState([]);
+    const [lancamento, setLancamento] = useState([]);
 
-    useEffect(()=>{
-        async function loadFilmes(){
+    useEffect(() => {
+        async function loadFilmes() {
             const response = await api.get("/movie/popular", {
-                params:{
+                params: {
                     api_key: "28fc232cc001c31e8a031f419d0a14ca",
                     language: "pt-BR",
                     page: 1,
-                   }
+                }
             })
             setFilmes(response.data.results.slice(0, 1));
         }
         loadFilmes();
-    },[]);
+    }, []);
+
+    useEffect(() => {
+
+        async function lancamento() {
+            const response = await api.get("movie/now_playing", {
+                params: {
+                    api_key: "28fc232cc001c31e8a031f419d0a14ca",
+                    language: "pt-BR",
+                    page: 1,
+                }
+            })
+
+            setLancamento(response.data.results.slice(0, 30));
+
+        }
+
+        lancamento();
+
+    }, [])
 
     return (
         <>
             <div className="destaque" >
-            {filmes.map((filme) => {
-               
-               return (
-                <>
-                <div className="legenda" >
-                    <h2>{filme.title}</h2>
-                    <p>{filme.overview}</p>
-                    <p>Avalição: {filme.vote_average}</p>
-                    <div class="branco"></div>
-                </div>
-                
-               <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
-               </>
-               );
-            }
-            )}
-           </div>
+                {filmes.map((filme) => {
+
+                    return (
+                        <>
+                            <div className="legenda" >
+                                <h2>{filme.title}</h2>
+                                <p>{filme.overview}</p>
+                                <p>Avalição: {filme.vote_average}</p>
+                                <div class="branco"></div>
+                            </div>
+
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+                        </>
+                    );
+                }
+                )}
+            </div>
+
+        
             <h3>Lançamentos</h3>
+            <div className="area-filme">
+                <div className="area-filme-item" >
+                    {lancamento.map((lancamentos) => {
+                        return (
+                            <img src={`https://image.tmdb.org/t/p/original/${lancamentos.poster_path}`} alt={lancamento.title} />
+                        )
+                    })}
+                </div>
+            </div>
+
+
             <h3>Lançamentos</h3>
             <h3>Lançamentos</h3>
         </>
